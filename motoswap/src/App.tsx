81b006b8cc2929bb5068e7ec14cd572da65dcef2
nietovaca <nietovaca@gmail.com>
2022-03-28 18:43:53 -0600

    imported dependencies styles and icons

--- motoswap/src/App.tsx
+++ motoswap/src/App.tsx
@@ -1,4 +1,16 @@
 import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+//Components
+import Drawer from '@mui/material/Drawer';
+import LinearProgress from '@mui/material/LinearProgress';
+import Grid from '@mui/material/Grid';
+import Badge from '@mui/material/Badge';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+//Styles 
+import { Wrapper } from './App.styles';
 
 const App = () => {
   return (
