6e61732d58e99452ae11309f7ab0b975720e61b0
nietovaca <nietovaca@gmail.com>
2022-03-28 19:06:00 -0600

    cart item types and axios get from markdown

--- motoswap/src/App.tsx
+++ motoswap/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useQuery } from 'react-query'
+import axios from 'axios'
 
 //Components
 import Drawer from '@mui/material/Drawer';
@@ -12,7 +13,37 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 //Styles 
 import { Wrapper } from './App.styles';
 
+//Types 
+export type CartItemType = {
+  id: number; 
+  category: number; 
+  title: string; 
+  description: string; 
+  image: string; 
+  price: number; 
+  in_stock: boolean; 
+  is_active: boolean; 
+  created: string;
+  updated: string; 
+}
+
 const App = () => {
+  let [products, setProducts] = useState([])
+
+  const getProducts = () => {
+    axios
+      .get('http://backcap.herokuapp.com/api/products')
+      .then(
+        (response) => setProducts(response.data),
+        (err) => console.log(err)
+      )
+      .catch((error) => console.error(error))
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <div className="App">
     Start 
