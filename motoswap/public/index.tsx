28e32dfb45b614e39b9eef6306fbca2f929006a2
nietovaca <nietovaca@gmail.com>
2022-03-31 16:52:40 -0600

    tweaking tsconfig

--- motoswap/public/index.tsx
+++ motoswap/public/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../src/App';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const client = new QueryClient(); 
